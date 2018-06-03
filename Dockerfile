12e06f63d9b60d87f9d86ec854fb1fa3bcb5856c
Sebastian Krysmanski <noreply@manski.net>
2018-06-03 15:43:12 +0200

    Fixed security hole on macOS
    Docker on macOS lies about the file ownership.

--- Dockerfile
+++ Dockerfile
@@ -9,9 +9,12 @@ COPY files/sshd_config /etc/ssh/
 RUN apt-get update && apt-get install -y openssh-server
 EXPOSE 22
 
-RUN adduser anonymous --disabled-password --system && passwd -d anonymous
+RUN adduser anonymous --system && passwd -d anonymous
 
 VOLUME ["/app"]
+
+# Add user for checking permissions on MacOS volumes (see entrypoint script)
+RUN adduser permcheck --system && usermod --shell /bin/bash permcheck
 
 COPY docker-entrypoint.sh /
 RUN chmod +x /docker-entrypoint.sh
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,20 @@ set -e
 
 if [ ! -f '/app/ssh_host_rsa_key' ]; then
     ssh-keygen -t rsa -N "" -f /app/ssh_host_rsa_key
+
+    # Fix file ownership on MacOS
+    # See: https://stackoverflow.com/a/43213455/614177
+    PERMCHECK=$(su permcheck -c 'stat -c "%U" /app/ssh_host_rsa_key')
+    if [ ! "$PERMCHECK" = "root" ]; then
+        chown root:root /app/ssh_host_rsa_key
+        chown root:root /app/ssh_host_rsa_key.pub
+
+        PERMCHECK=$(su permcheck -c 'stat -c "%U" /app/ssh_host_rsa_key')
+        if [ ! "$PERMCHECK" = "root" ]; then
+            echo "ERROR: Could not fix file ownership of '/app/ssh_host_rsa_key'"
+            exit 1
+        fi
+    fi
 fi
 
 exec /usr/sbin/sshd -D
